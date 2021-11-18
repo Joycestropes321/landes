f52c851021d05cd109cefd3565b47720541f9e57
Bolade Akinniyi <boladebode@gmail.com>
2021-11-18 13:59:57 +0100

    cors issue

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,4 @@
-<?php
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
+<?php 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AccountController;
