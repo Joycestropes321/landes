41f2ca9915b9f55c00fab5f1b3dde60b6c4b1e23
Bolade Akinniyi <boladebode@gmail.com>
2021-11-19 19:53:51 +0100

    https issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
+            URL::forceSchema('https');
         } 
         Blade::directive('formatMoney', function ($money, $fractional=false) {
             if ($fractional) {
