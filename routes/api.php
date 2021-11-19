ea02762fd9ffecf2745dfa0d167adb238e6baeda
Bolade Akinniyi <boladebode@gmail.com>
2021-11-19 19:05:37 +0100

    cors issue

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,4 @@
-<?php
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
+<?php 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
