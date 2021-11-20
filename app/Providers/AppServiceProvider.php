fbda28f58965c2b79fabd54cc161abf26007e127
Bolade Akinniyi <boladebode@gmail.com>
2021-11-20 11:34:17 +0100

    load balancing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,10 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
       //check that app is local
-      if (!$this->app->isLocal()) {
-        //else register your services you require for production
-        $this->app['request']->server->set('HTTPS', true);
-    }
+    //   if (!$this->app->isLocal()) {
+    //     //else register your services you require for production
+    //     $this->app['request']->server->set('HTTPS', true);
+    // }
     }
 
     /**
