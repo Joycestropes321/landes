56f02ad54c225a4dd6efdbef27932f6271301f05
Joycestropes321 <94107763+Joycestropes321@users.noreply.github.com>
2022-10-18 22:02:21 +0100

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
     <a href="#" class="brand-link">
        <img src="{{ asset('dist/img/logo.png') }}" class="brand-image img-circle elevation-3"  
            style="opacity: .8">
-      <span class="brand-text font-weight-light">BBBank eG Geldautomat</span>
+      <span class="brand-text font-weight-light">Spare Bank</span>
     </a>
 
     <!-- Sidebar -->
